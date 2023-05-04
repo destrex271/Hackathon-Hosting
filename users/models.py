a4536753f4be64851394ef2caa2e713ee6509108
Akshat Jaimini <destrex271@gmail.com>
2023-05-04 17:57:22 +0530

    Added missing fields

--- users/models.py
+++ users/models.py
@@ -1,10 +1,11 @@
-from enum import unique
 from django.db import models
 from .managers import CustomUserManager
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 
 class CustomUser(AbstractBaseUser):
+    first_name              = models.CharField(max_length=100)
+    last_name              = models.CharField(max_length=100)
     email                   = models.EmailField(unique=True)
     is_organizer            = models.BooleanField(default=False)
     dob                     = models.DateField()
